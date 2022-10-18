575fd1c105601e3183219c813b11327586b9b319
sasashu <112250615+sasashu@users.noreply.github.com>
2022-10-18 09:20:46 +0000

    [Update]ルート名称調整、見直し

--- config/routes.rb
+++ config/routes.rb
@@ -1,52 +1,37 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-    get 'orders/show'
+    resources :orders, only:[:show, :update] do
+      resources :order_details, only:[:update]
+    end
+
+    resources :customers, only:[:index, :show, :edit, :update]
+
+    resources :genres, only:[:index, :edit, :create, :update]
+
+    resources :items, except: [:destroy]
+
+    get '/' =>'homes#top'
   end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-  end
-  namespace :public do
-    get 'orders/comfirm'
-    get 'orders/complete'
-    get 'orders/new'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/unsubscribe'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
+
+  scope module: :public do
+    resources :addresses, only:[:index, :edit, :create, :update, :destroy]
+
+    post 'orders/comfirm',as: 'comfirm'
+    get 'orders/complete',as: 'complete'
+    resources :orders, only:[:new, :index, :show, :create]
+
+    delete 'cart_items/destroy_all'=>'cart_items#destroy_all',as: 'destroy_all'
+    resources :cart_items, only:[:index, :create, :update, :destroy]
+
+    patch 'customers/withdraw',as: 'withdraw'
+    get 'customers/unsubscribe',as: 'unsubscribe'
+    resource :customers, only:[:show, :edit, :update]
+
+    resources :items, only:[:index, :show]
+
+    root :to =>"homes#top"
+    get '/about'=>'homes#about',as: 'about'
   end
   # 顧客用
   # URL /customers/sign_in ...

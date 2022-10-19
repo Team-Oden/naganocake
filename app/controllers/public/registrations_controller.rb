33f2bc886f9c35149e465155b46b4015c3f8f05e
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-10-19 17:07:49 +0900

    76行目のtelephone_numberのあとの[,]削除しました

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -73,7 +73,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   
   protected
    def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name,:first_name,:family_kana,:first_kana,:postal_code,:address,:telephone_number,])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name,:first_name,:family_kana,:first_kana,:postal_code,:address,:telephone_number])
   end
   
 end

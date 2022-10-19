5e614165a7be97bd86499636e85047fa4a128266
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-10-19 07:54:46 +0000

    ヘッダー枠組み作成

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -25,6 +25,7 @@ class Admin::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
   protected
+  
   def after_sign_in_path_for(resource)
     admin_path
   end

e44bf4c31a61b640c87b2d68ad737bfc0ef43e6b
Ayaka <ec2-user@ip-172-31-4-30.ap-northeast-1.compute.internal>
2022-10-19 09:09:09 +0000

    [Add]P.マイページ・編集機能の追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,14 +1,18 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def unsubscribe
   end
 
   def update
+    @customer = current_customer
+    redirect_to my_page_url
   end
 
   def withdraw

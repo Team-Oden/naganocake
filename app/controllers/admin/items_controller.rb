14e5c37b3eda78e1fc4ddf73e1652c37ba1585e4
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-10-20 05:48:28 +0000

    新規登録機能のみ完成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,20 +1,43 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items =Item.page(params[:page]).per(10)
   end
 
   def new
+    @item = Item.new
+    @genres = Genre.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item.id)
+    else
+      render :new
+    end
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :price, :is_active, :introduction, :image)
+  end
 end

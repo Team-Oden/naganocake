d52af79d1ae72dbd1d740248e4114954a6ba39a0
sasashu <112250615+sasashu@users.noreply.github.com>
2022-10-20 12:10:18 +0000

    [Update]配送先設定中

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,8 +1,11 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = current_customer
+    @new_address = Address.new
   end
 
   def edit
+    @address = current_customer
   end
 
   def create

a7ec91292c7535e22c604c6bac1652c83c5b56a0
Ayaka <ec2-user@ip-172-31-4-30.ap-northeast-1.compute.internal>
2022-10-20 01:24:13 +0000

    [add]エラーメッセージを追加しました

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -12,8 +12,11 @@ class Public::CustomersController < ApplicationController
 
   def update
     @customer = current_customer
-    @customer.update(customer_params)
-    redirect_to my_page_path
+    if @customer.update(customer_params)
+      redirect_to my_page_path
+    else
+      render :edit
+    end
   end
 
   def withdraw

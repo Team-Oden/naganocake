68e07a151bf7eadd7c7b9b2c8efeffa10a53f446
sasashu <112250615+sasashu@users.noreply.github.com>
2022-10-20 06:22:03 +0000

    [Update]admin会員一覧

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,14 +1,32 @@
 class Admin::CustomersController < ApplicationController
+  before_action :authenticate_admin!
+  
   def index
+    @customers = Customer.all
   end
 
   def show
+    # @customer = Customer.find(params[:id])
+    @customer = current_customer
   end
 
   def edit
+    # @customer = Customer.find(params[:id])
+    @customer = current_customer
   end
 
   def update
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer), notice: "会員情報を編集しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:family_name, :first_name, :family_kana, :first_kana, :postal_code, :telephone_number, :address, :email, :is_active)
   end
 
 end
--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,6 +1,5 @@
 class Admin::GenresController < ApplicationController
-  # ↓ログインできるまで切ってあります↓
-  # before_action :authenticate_admin!
+  before_action :authenticate_admin!
 
   def index
     @genre = Genre.new
@@ -16,7 +15,7 @@ class Admin::GenresController < ApplicationController
     if @genre.save
       redirect_to admin_genres_path
     else
-      @renres = Genre.all
+      @genres = Genre.all
       render :index
     end
   end
@@ -27,6 +26,7 @@ class Admin::GenresController < ApplicationController
     redirect_to admin_genres_path
   end
 
+  private
   def genre_params
     params.require(:genre).permit(:name)
   end

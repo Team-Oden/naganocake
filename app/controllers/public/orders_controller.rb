751a09f64a975fc9673ac07b0e0479b1af84c275
EC2 Default User <ec2-user@ip-172-31-9-195.ap-northeast-1.compute.internal>
2022-10-24 08:01:06 +0000

    注文確認画面エラー解決

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,6 +1,4 @@
 class Public::OrdersController < ApplicationController
-  before_action :authenticate_customer!
-
   def comfirm
     @order = Order.new(order_params)
     if params[:order][:address_number] == "1"
@@ -9,7 +7,7 @@ class Public::OrdersController < ApplicationController
       @order.postal_code = current_customer.postal_code
 
     elsif params[:order][:address_number] == "2"
-      if Address.exists?(name: params[:order][:registered])
+      if Address.exists?(params[:order][:registered])
         @order.name = Address.find(params[:order][:registered]).name
         @order.address = Address.find(params[:order][:registered]).address
         @order.postal_code = Address.find(params[:order][:registered]).postal_code
@@ -41,6 +39,7 @@ class Public::OrdersController < ApplicationController
   def show
     @orders = current_customer.orders
     @order = current_customer.orders.find(params[:id])
+    @cart_items = current_customer.cart_items
   end
 
   def create
